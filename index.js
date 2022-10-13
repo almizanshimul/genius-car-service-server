171bcd9aeaa2744d06ce66e3b2596c9cb439ef19
Al Mizan <67356360+almizanshimul@users.noreply.github.com>
2022-10-13 16:53:31 +0600

    first step added

---
+++ index.js
@@ -0,0 +1,80 @@
+const express = require('express')
+const cors = require('cors')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config()
+const app = express()
+const port = process.env.PORT || 4000
+
+//middleware
+
+app.use(cors())
+app.use(express.json())
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hcjatm5.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const serviceCollection = client.db('geniusCar').collection('service');
+        const orderCollection = client.db('geniusCar').collection('order');
+
+        // get all services data 
+        app.get('/service', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services)
+        })
+        // get one service data 
+        app.get('/service/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) }
+            const service = await serviceCollection.findOne(query)
+            res.send(service)
+        })
+
+        // POST 
+        app.post('/service', async (req, res) => {
+            const newService = req.body;
+            const result = await serviceCollection.insertOne(newService)
+            res.send(result)
+        })
+        // Delete one service data 
+        app.delete('/service/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) }
+            const service = await serviceCollection.deleteOne(query)
+            res.send(service)
+        })
+
+        //order collection api
+        app.post('/order', async(req,res)=>{
+            const order = req.body
+            const result =await orderCollection.insertOne(order)
+            res.send(result)
+        })
+        //order all collection api
+        app.get('/orders', async(req,res)=>{
+            const query = req.query;
+            const cursor = orderCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services)
+            console.log(services);
+        })
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.dir)
+
+
+app.get('/', (req, res) => {
+    res.send('Running Genius Server')
+})
+
+app.listen(port, () => {
+    console.log('Listening port: ', port);
+})
---
+++ step.js
@@ -0,0 +1,33 @@
+/**
+ * Create Server side: genius car services server
+ * --------------------------------------------------------
+ * 1. create a folder
+ * 2. open folder and cmd, then npm init -y
+ * 3. create index.js file
+ * 4. tarpore package.json file start script add korte hobe auto restart korar jonno start-dev script add korte hobe
+ * 5. npm i express cors mongodb dotenv
+ * 6. .env file toyri korte hobe
+ * 7. akta server toyri korechi port: 4000
+ * 
+ * 
+ * -----------------------------------------------
+ * connected database
+ * -----------------------------------------------------------
+ * 1. first create a database and insert previous data 
+ * 2. copy connect cluster code and paste into index.js file
+ * 3. Create a new user then copy user name and pass
+ * 4. Create secret user and pass with .env file
+ * 5. Then input index uri line secret user and pass 
+*
+*-----------------------------------------------
+*Here used
+*-----------------------------------------------
+*1. React
+*2. React Router
+*3. Firebase
+*4. Bootstrape
+*5. Firebase hooks
+*6. React Hook form
+*7. Toastify
+*8. React Icons
+ */
